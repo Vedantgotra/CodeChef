b313c2e6fab6f0bbcb19536d1dee6c6cf45c2174
Vedant Gotra <88156427+Vedantgotra@users.noreply.github.com>
2022-08-03 21:52:57 +0530

    Add files via upload

---
+++ FIT.java
@@ -0,0 +1,21 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int X;
+		Scanner sc=new Scanner(System.in);
+		int T=sc.nextInt();
+		for(int i=0;i<T;i++){
+		    X=sc.nextInt();
+		    System.out.println(X*10);
+		}
+	}
+}
